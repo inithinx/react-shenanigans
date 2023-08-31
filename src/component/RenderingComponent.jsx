f18cc58c88aab25d4b84066e5f0f115fda4050b8
Nithin Krishnaa <info@nithinkrishna.in>
2023-08-31 15:51:18 +0530

    wtf part 3

--- src/component/RenderingComponent.jsx
+++ src/component/RenderingComponent.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
 export default class RenderingComponent extends Component {
-    constructor(props){ super(props) }
+    constructor(props){ super(props) 
+        this.state = {
+            text: "condition",
+            show: false,
+            fruits: ["Apple", "Orange", "Banana", "Mango", "Strawberry"]
+        }
+    }
+    changeShow(){ this.setState({show: !this.state.show}) }
+
     render() {
+        var variable = this.state.show ? 'Hide': 'Show'
         return (
-            <div>RenderingComponent</div>
+            <>
+            <ul>
+                { this.state.fruits.map((fruit,index) => {return <li>{index}:{fruit}</li>}) }
+            </ul>
+            <div> Rendering Component: </div>
             {this.state.show && this.state.text}
-            <button onClick={()=> this.changeShow()}>{this.state.show ? }
-                </button>
+            <button onClick={()=> this.changeShow()}>{variable}</button>
+            </>
         )
     }
 }
